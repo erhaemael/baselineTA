999214760828bade1f11ca1af3dbba62e9ca146d
erhaemael <rahma.alia.tif421@polban.ac.id>
2025-02-10 19:41:07 +0700

    first commit

---
+++ preprocess.py
@@ -0,0 +1,16 @@
+import os
+from src.data.WESAD.preprocess import main as preprocess_wesad
+# from src.data.DREAMER.preprocess import main as preprocess_dreamer
+# from src.data.HCI.preprocess import main as preprocess_hci
+
+
+def main():
+    # Make preprocessed directory
+    os.makedirs("data/preprocessed", exist_ok=True)
+
+    # Preprocess WESAD dataset
+    preprocess_wesad()
+
+
+if __name__ == "__main__":
+    main()
